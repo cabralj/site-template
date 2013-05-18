d9c27b6d96854d4e2264d7ff1c89e4e9b40f9039
cabral <cabralj@gmail.com>
2013-05-18 17:09:57 -0400

    created comp directory

---
+++ comp/feature.php
@@ -0,0 +1,23 @@
+      <div class="featurette">
+        <img class="featurette-image pull-right" src="./img/examples/browser-icon-chrome.png">
+        <h2 class="featurette-heading">eBook Platform</h2>
+        <p class="lead">Interactive ebooks designed for use in the publishers, teachers and students.</p>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="featurette">
+        <img class="featurette-image pull-left" src="./img/examples/browser-icon-firefox.png">
+        <h2 class="featurette-heading">Mobile App Factory <span class="muted">See for yourself.</span></h2>
+        <p class="lead"></p>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="featurette">
+        <img class="featurette-image pull-right" src="./img/examples/browser-icon-safari.png">
+        <h2 class="featurette-heading">And lastly, this one. <span class="muted">Checkmate.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+
+      <hr class="featurette-divider">
