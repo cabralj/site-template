bdd9000124dcc843517a176d5aaf9ce6ae4dbbaf
cabral <cabralj@gmail.com>
2013-05-18 16:52:43 -0400

    adding base

---
+++ games.php
@@ -0,0 +1,29 @@
+<!-- GAMES -->
+<div class="container games">
+
+<!-- Three columns of text below the carousel -->
+<div class="row">
+  <div class="span4">
+    <img class="img-circle" data-src="holder.js/140x140">
+    <h2>Heading</h2>
+    <p>The quick brown fox jumped over the lazy dog. </p>
+    <p><a class="btn" href="#">View details &raquo;</a></p>
+  </div><!-- /.span4 -->
+  <div class="span4">
+    <img class="img-circle" data-src="holder.js/140x140">
+    <h2>Heading</h2>
+    <p>The quick brown fox jumped over the lazy dog. </p>
+    <p><a class="btn" href="#">View details &raquo;</a></p>
+  </div><!-- /.span4 -->
+  <div class="span4">
+    <img class="img-circle" data-src="holder.js/140x140">
+    <h2>Heading</h2>
+    <p>The quick brown fox jumped over the lazy dog. </p>
+    <p><a class="btn" href="#">View details &raquo;</a></p>
+  </div><!-- /.span4 -->
+
+</div><!-- /.row -->
+
+
+<!--Divider -->
+<hr class="featurette-divider">
